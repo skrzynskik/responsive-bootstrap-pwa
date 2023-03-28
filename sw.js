7213d70e39be08337abbef2f3c948eb6d3ee966a
skrzynski.k <kacperskrzynski@outlook.com>
2023-03-28 02:13:38 +0200

    added sw and pwa support

---
+++ sw.js
@@ -0,0 +1,21 @@
+const bootstrapPwaExample = "bootstrap-pwa-example"
+const assets = [
+  "/",
+  "/index.html",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(bootstrapPwaExample).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
